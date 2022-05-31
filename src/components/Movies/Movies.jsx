ec3e06e94ad75ed166288f654826d058ee23ecb4
snlaight <snlaight10@gmail.com>
2022-05-31 15:42:28 +0200

    feat: fix eslint, add redux store & redux api, implement api fetching for movies and genres

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='4rem' />
+      </Box>
+    );
+  }
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
   return (
     <div>
       <h1>Movies</h1>
---
+++ src/services/TMDB.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3',
+  }),
+  endpoints: (builder) => ({
+    //*get genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+    //* get movies by [type]
+    getMovies: builder.query({
+      query: () => {
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
