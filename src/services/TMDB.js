eadc8c62d2fa1ffb6c29a8989c34aa1621df6a6e
snlaight <snlaight10@gmail.com>
2022-06-07 10:48:11 +0200

    feat: adding switch genre, search functionality

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
@@ -15,7 +14,11 @@ export const tmdbApi = createApi({
     }),
     //* get movies by [type]
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //* get movies by search
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
         //* get movies by category
         if (
           genreIdOrCategoryName &&
